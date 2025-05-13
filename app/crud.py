4fc27d2b3aafb396e189b5c56a8c36f7a811fa0b
hanzi-code <195461519+hanzi-code@users.noreply.github.com>
2025-05-13 16:38:27 -0300

    Edit crud.py and models.py add some template models and created a function to get all the todos

---
+++ app/crud.py
@@ -0,0 +1,4 @@
+from .models import todos
+
+def get_all_todos():
+    return todos
---
+++ app/models.py
@@ -0,0 +1,4 @@
+todos = [
+        {"id": 1, "title": "TODO 1", "completed": False},
+        {"id": 2, "title": "TODO 2", "completed": True},
+        ]
