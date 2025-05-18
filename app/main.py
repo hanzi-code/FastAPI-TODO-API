f04c29b80af68c066e376dc884de323bf6a51fe6
hanzi-code <195461519+hanzi-code@users.noreply.github.com>
2025-05-18 20:34:22 -0300

    Edit main.py add options now to post put and delete the todos

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
-from app import crud
+from fastapi import FastAPI, HTTPException
+from . import crud
+from .schemas import TodoCreate, Todo
+
 
 app = FastAPI()
 
@@ -7,6 +9,24 @@ app = FastAPI()
 def read_root():
     return {"message": "Bem-vindo à API TODO"}
 
-@app.get("/todos")
+@app.get("/todos", response_model = list[Todo])
 def get_todos():
     return crud.get_all_todos()
+
+@app.post("/todos", response_model = Todo, status_code = 201)
+def create_todo(todo: TodoCreate):
+    return crud.create_todo(todo)
+
+@app.put("/todos/{todo_id}", response_model = Todo)
+def complete_todo(todo_id: int):
+    updated = crud.update_todo(todo_id)
+    if not updated:
+        raise HTTPException(status_code = 404, detail = "TODO não encontrado")
+    return updated
+
+@app.delete("/todos/{todo_id}", response_model = Todo)
+def delete_todo(todo_id: int):
+    deleted = crud.delete_todo(todo_id)
+    if not deleted:
+        raise HTTPException(status_code = 404, detail = "TODO não encontrado")
+    return deleted
