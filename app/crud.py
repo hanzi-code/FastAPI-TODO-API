09d4a7a32c15d30c91b923cddc71031c07bcdcd4
hanzi-code <195461519+hanzi-code@users.noreply.github.com>
2025-06-02 10:49:16 -0300

    lógica de banco de dados

--- app/crud.py
+++ app/crud.py
@@ -1,24 +1,27 @@
-from .models import todos
-from .schemas import Todo, TodoCreate
+from sqlalchemy.orm import Session
+from . import models, schemas
 
 def get_all_todos():
-    return todos
+    return db.query(models.Todo).all()
 
-def create_todos(todo_data: TodoCreate):
-    new_id = max(todo["id"] for todo in todos) + 1 if todos else 1
-    new_todo = {"id": new_id, "title": todo_data.title, "completed": False}
-    todos.append(new_todo)
-    return new_todo
+def create_todos(db: Session, todo: schemas.TodoCreate):
+    db_todo = models.Todo(title=todo.title)
+    db.add(db_todo)
+    db.commit()
+    db.refresh(db_todo)
+    return db_todo
 
-def update_todo(todo_id: int):
-    for todo in todos:
-        if todo["id"] == todo_id:
-            todo["completed"] = True
-            return todo
-    return None
+def update_todo(db: Session, todo_id: int):
+    db_todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    if db_todo:
+        db_todo.completed = False
+        db.commit()
+        db.refresh(db_todo)
+    return db_todo
 
-def delete_todo(todo_id: int):
-    for i, todo in enumerate(todos):
-        if todo[id] == todo_id:
-            return todos.pop(i)
-    return None
+def delete_todo(db: Session, todo_id: int):
+    db_todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    if db_todo:
+        db.delete(db_todo)
+        db.commit()
+    return db_todo
